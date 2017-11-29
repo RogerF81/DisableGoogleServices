5a583749325224678a6582c3ce1fc9b329bcdf3a
RogerF81 <lowhouse123@gmail.com>
2017-11-29 14:27:40 +0100

    Create service.sh

---
+++ common/service.sh
@@ -0,0 +1,48 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
+su -c "pm disable com.google.android.gms.update.SystemUpdateActivity"
+su -c "pm disable com.google.android.gms.update.phone.SystemUpdateActivity"
+su -c "pm disable com.google.android.gms.update.SystemUpdateService"
+su -c "pm disable com.google.android.gms.update.SystemUpdateServiceActiveReceiver"
+su -c "pm disable com.google.android.gms.update.SystemUpdateServiceReceiver"
+su -c "pm disable com.google.android.gms.update.SystemUpdateServiceSecretCodeReceiver"
+su -c "pm disable com.google.android.gms.ads.AdRequestBrokerService"
+su -c "pm disable com.google.android.gms.ads.identifier.service.AdvertisingNotificationService"
+su -c "pm disable com.google.android.gms.ads.social.GcmSchedulerWakeupService"
+su -c "pm disable com.google.android.gms.ads.measurement.GmpConversionTrackingBrokerService"
+su -c "pm disable com.google.android.gms.ads.GservicesValueBrokerService"
+su -c "pm disable com.google.android.gms.ads.config.FlagsReceiver"
+su -c "pm disable com.google.android.gms.ads.config.GServicesChangedReceiver"
+su -c "pm disable com.google.android.gms.ads.jams.SystemEventReceiver"
+su -c "pm disable com.google.android.gms.ads.settings.AdsSettingsActivity"
+su -c "pm disable com.google.android.gms.ads.jams.NegotiationService"
+su -c "pm disable com.google.android.gms.ads.adinfo.AdvertisingInfoContentProvider"
+su -c "pm disable com.google.android.gms.analytics.service.AnalyticsService"
+su -c "pm disable com.google.android.gms.analytics.AnalyticsService"
+su -c "pm disable com.google.android.gms.analytics.AnalyticsTaskService"
+su -c "pm disable com.google.android.gms.analytics.AnalyticsReceiver"
+su -c "pm disable com.google.android.gms.analytics.internal.PlayLogReportingService"
+su -c "pm disable com.google.android.gms.feedback.FeedbackAsyncService"
+su -c "pm disable com.google.android.gms.feedback.LegacyBugReportService"
+su -c "pm disable com.google.android.gms.feedback.OfflineReportSendTaskService"
+su -c "pm disable com.google.android.gms.feedback.SendService"
+su -c "pm disable com.google.android.gms.feedback.AnnotateScreenshotActivity"
+su -c "pm disable com.google.android.gms.feedback.FeedbackActivity"
+su -c "pm disable com.google.android.gms.feedback.IntentListenerFeedbackActivity"
+su -c "pm disable com.google.android.gms.feedback.PreviewActivity"
+su -c "pm disable com.google.android.gms.feedback.PreviewScreenshotActivity"
+su -c "pm disable com.google.android.gms.feedback.ShowTextActivity"
+su -c "pm disable com.google.android.gms.feedback.SuggestionsActivity"
+#su -c "pm disable com.google.android.location.reporting.service.LocationHistoryInjectorService"
+#su -c "pm disable com.google.android.location.internal.AnalyticsSamplerReceiver"
+#locationsharing and measurement services perhaps to add in future
+su -c "pm disable com.google.android.gsf/.update.SystemUpdateActivity"
+su -c "pm disable com.google.android.gsf/.update.SystemUpdatePanoActivity"
+su -c "pm disable com.google.android.gsf/.update.SystemUpdateService"
+su -c "pm disable com.google.android.gsf/.update.SystemUpdateService$Receiver"
+su -c "pm disable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver"
